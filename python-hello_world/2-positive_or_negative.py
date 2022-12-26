1c6324d0f4fa417c465e9cc29dee79f8e9b816a2
HarishChalla1 <harishch@brightcomgroup.com>
2022-12-26 05:32:12 +0000

    tsk 1

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,9 +1,12 @@
-#!#!/usr/bin/python3
 #!/usr/bin/python3
-word = "Holberton"
-word_first_3= word[0:3]
-word_last_2word[-2:]
-middle_word=word[1:-1]
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number>0:
+    print("number is positive")
+if number==0:
+    print("number is zero")
+if number<0:
+    print("number is negative")
+
+
